1a1b5eb2bd5b1fa1416295491a85b55542bca19a
Binghamton Tech Collective <techcollective@binghamtonsa.org>
2024-10-07 15:05:13 -0400

    Created section for Survey form, Projects, and Get Connected on the Home Page. Changed the favicon of the website

--- src/Components/Home/AboutUsSection.js
+++ src/Components/Home/AboutUsSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GetConnected from "./GetConnected.js";
 
 const Home = () => {
   return (
@@ -9,29 +10,41 @@ const Home = () => {
           <img
             src="/btc_logo.png"
             alt="BTC Logo"
-            className="w-full object-center justify-start h-80"
+            className="w-full object-center justify-start h-80 z-0 inset-0 top-10 md:absolute"
           />
+          <img
+            src="/btc_element.png"
+            alt="BTC Element Logo"
+            className="w-full justify-start z-10 relative"
+          ></img>
         </div>
-        <div className="py-8 bg-gray-100">
-          <div className="container mx-auto px-4 flex flex-col md:flex-row">
-            <div className="w-full md:w-3/5 md:pr-12 mb-6 md:mb-0">
-              <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">
-                About Us
-              </h2>
-              <p className="text-gray-700 mb-6 text-base md:text-lg">
-                Welcome to the Binghamton Tech Collective, a community of tech
-                enthusiasts, developers, and innovators. We are dedicated to
-                fostering a collaborative environment where individuals can
-                learn, grow, and contribute to the tech community. Our mission
-                is to create innovative products that positively impact the
-                students at Binghamton University.
-              </p>
-              <Link
-                to="/join"
-                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 md:py-3 md:px-6 text-base md:text-lg inline-flex items-center rounded-md"
-              >
-                Join Now→
-              </Link>
+        <div className="py-8 bg-white relative">
+          <img
+            src="/about_us_img.png"
+            alt="BTC Background"
+            className="absolute inset-0 top-0 md:h-80 sm:h-40 md:top-8 object-cover z-0"
+          ></img>
+          <div className="container mx-auto px-4 flex flex-col md:flex-row relative">
+            <div className="w-full md:w-3/5 md:pr-12 mb-6 md:mb-0 relative z-10">
+              <div className="relative">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">
+                  About Us
+                </h2>
+                <p className="text-gray-700 mb-6 text-base md:text-lg">
+                  Welcome to the Binghamton Tech Collective, a community of tech
+                  enthusiasts, developers, and innovators. We are dedicated to
+                  fostering a collaborative environment where individuals can
+                  learn, grow, and contribute to the tech community. Our mission
+                  is to create innovative products that positively impact the
+                  students at Binghamton University.
+                </p>
+                <Link
+                  to="/join"
+                  className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 md:py-3 md:px-6 text-base md:text-lg inline-flex items-center rounded-md"
+                >
+                  Join Now→
+                </Link>
+              </div>
             </div>
             <div className="w-full md:w-2/5 md:pl-8">
               <img
@@ -42,8 +55,48 @@ const Home = () => {
             </div>
           </div>
         </div>
-        {/* Remaining space */}
-        <div className="flex-grow"></div>
+        <div className="bg-custom-green flex flex-row">
+          <iframe
+            title="survey"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSd0RL42AHTrJjDx6tKFpGYcIE5cg2e__42jKuXYB-3AeYQDKw/viewform?embedded=true"
+            width="100%"
+            height="850"
+            style={{ border: "none", margin: 0, padding: 0 }}
+          >
+            Loading…
+          </iframe>
+        </div>
+        <div className="container mx-auto px-4 flex flex-col md:flex-row py-8 bg-white">
+          <div className="w-full md:w-2/5 md:pr-8">
+            <img
+              src="/btc_team.jpg"
+              alt="Our Team"
+              className="shadow-lg w-full h-auto ml-auto"
+            />
+          </div>
+          <div className="w-full md:w-3/5 md:pl-6 mb-6 md:mb-0 relative">
+            <div className="relative py-4">
+              <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black">
+                Our Projects
+              </h2>
+              <p className="text-gray-700 mb-6 text-base md:text-lg">
+                Welcome to the Binghamton Tech Collective, a community of tech
+                enthusiasts, developers, and innovators. We are dedicated to
+                fostering a collaborative environment where individuals can
+                learn, grow, and contribute to the tech community. Our mission
+                is to create innovative products that positively impact the
+                students at Binghamton University.
+              </p>
+              <Link
+                to="/"
+                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 md:py-3 md:px-6 text-base md:text-lg inline-flex items-center rounded-md"
+              >
+                Try it Now→
+              </Link>
+            </div>
+          </div>
+        </div>
+        <GetConnected />
       </div>
     </>
   );
