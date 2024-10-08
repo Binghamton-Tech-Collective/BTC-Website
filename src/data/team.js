4a70eecc69a380d35cdd5394306646ef67e118f3
ron103 <waghmarerohan30@gmail.com>
2024-10-08 16:58:36 -0400

    added code changes to display details of team members

---
+++ src/data/team.js
@@ -0,0 +1,21 @@
+// teamData.js
+import pfp from './blank.png';
+
+export const teamMembers = [
+  { name: 'Lucy Tien-Pham', position: 'President', team: 'Executive Board', image: pfp },
+  { name: 'John Doe', position: 'Vice President', team: 'Executive Board', image: pfp },
+  { name: 'Jane Smith', position: 'Secretary', team: 'Executive Board', image: pfp },
+  { name: 'Michael Brown', position: 'Treasurer', team: 'Executive Board', image: pfp },
+  { name: 'Emily White', position: 'Product Manager', team: 'Product Management Team', image: pfp },
+  { name: 'David Green', position: 'Lead Developer', team: 'Product Management Team', image: pfp },
+  { name: 'Sophia Blue', position: 'UX Designer', team: 'Product Management Team', image: pfp },
+  { name: 'Chris Red', position: 'QA Engineer', team: 'Product Management Team', image: pfp },
+  { name: 'Lucy Tien-Pham', position: 'President', team: 'Software Engineering Team', image: pfp },
+  { name: 'John Doe', position: 'Vice President', team: 'Software Engineering Team', image: pfp },
+  { name: 'Jane Smith', position: 'Secretary', team: 'Software Engineering Team', image: pfp },
+  { name: 'Michael Brown', position: 'Treasurer', team: 'Software Engineering Team', image: pfp },
+  { name: 'Emily White', position: 'Product Manager', team: 'Software Engineering Team', image: pfp },
+  { name: 'David Green', position: 'Lead Developer', team: 'UI/UX Team', image: pfp },
+  { name: 'Sophia Blue', position: 'UX Designer', team: 'UI/UX Team', image: pfp },
+  { name: 'Chris Red', position: 'QA Engineer', team: 'UI/UX Team', image: pfp },
+];
