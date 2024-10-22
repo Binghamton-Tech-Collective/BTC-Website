60e32df1f2e8cad91e846822dc35fbcb33c7350c
Blake <blakeabrenica@gmail.com>
2024-10-22 16:06:47 -0400

    Fixed padding, btc logo picture now centered

--- src/Components/About/MissionOverview.js
+++ src/Components/About/MissionOverview.js
@@ -4,16 +4,16 @@ const About = () => {
   return (
     <>
       <div className="h-screen flex flex-col">
-        <div className="py-4 pt-16 relative bg-custom-green"> 
+      <div className="py-4 pt-16 relative bg-custom-green flex items-center justify-center"> 
           <img
             src="/btc_logo.png"
             alt="BTC Logo"
-            className="w-full object-center justify-start h-80"
+            className="object-center h-80"
           />
         </div>
         <div className="py-8 bg-gray-100">
           <div className="container mx-auto flex flex-col md:flex-row">
-            <div className="w-full md:w-3/5 md:pr-12 mb-6 md:mb-0 ">
+            <div className="w-full md:w-3/5 md:pr-12 mb-6 md:mb-0 px-5 ">
               <h2 className="text-3xl md:text-4xl font-bold mb-4 text-black"> Our Mission</h2>
               <p className="text-gray-700 mb-6 text-base md:text-lg">
                 Mission Statement
@@ -31,7 +31,7 @@ const About = () => {
                 Goals Statement
               </p>
             </div>
-            <div className="w-full md:w-2/5 md:pl-8 ">
+            <div className="w-full md:w-2/5 md:pl-8 px-5 ">
               <img
                 src="/btc_team.jpg"
                 alt="Our Team"
